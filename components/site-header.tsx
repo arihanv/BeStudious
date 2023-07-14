b86ffeed39decd397fe328cb2a5b92b84781d492
arihanv <arihanvaranasi@gmail.com>
2023-07-13 21:32:40 -0500

    feat: Make Initial Files with Shadcn

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,16 +1,19 @@
-import Link from "next/link"
+// import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
+// import { siteConfig } from "@/config/site"
+// import { buttonVariants } from "@/components/ui/button"
+// import { Icons } from "@/components/icons"
+// import { MainNav } from "@/components/main-nav"
+// import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+      <div className="container flex h-16 items-center space-x-4 justify-center sm:space-x-0">
+        <div className="font-bold text-lg w-fit">
+          BeStudious.
+        </div>
+        {/* <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
@@ -45,7 +48,7 @@ export function SiteHeader() {
             </Link>
             <ThemeToggle />
           </nav>
-        </div>
+        </div> */}
       </div>
     </header>
   )
