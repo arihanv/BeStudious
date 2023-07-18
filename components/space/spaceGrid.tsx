1326f9df7d5d9877b68f0e2a1e0bfced149fdaab
arihanv <arihanvaranasi@gmail.com>
2023-07-17 22:58:35 -0500

    style: Center Spaces on Mobile View

--- components/space/spaceGrid.tsx
+++ components/space/spaceGrid.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function SpaceGrid({}: Props) {
   return (
-    <div className="grid w-full max-w-[900px] auto-cols-auto grid-cols-1 flex-wrap gap-5 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="flex w-full max-w-[900px] grid-cols-1 flex-wrap justify-center gap-5 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:justify-start">
       <Space />
       <Space />
       <Space />
--- components/space/uploadButton.tsx
+++ components/space/uploadButton.tsx
@@ -7,7 +7,7 @@ export default function UploadButton({}: Props) {
   return (
     <div className="fixed bottom-0 mb-5 w-fit rounded-xl p-2 shadow-sm shadow-black backdrop-blur-sm">
       <div className="flex w-full items-center gap-3">
-        <button className="flex flex-1 items-center justify-between gap-3 rounded-xl bg-blue-800 border-slate-900 border-4 p-2 px-3 transition ease-in-out hover:scale-110">
+        <button className="flex flex-1 items-center justify-between gap-3 rounded-xl border-4 border-slate-900 bg-blue-800 p-2 px-3 transition ease-in-out hover:scale-110">
           <div className="flex items-center gap-3 px-2">
             <PackagePlus />
             <div className="text-lg font-semibold">Make A Space</div>
