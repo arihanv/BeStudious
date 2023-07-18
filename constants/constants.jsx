c59c05b6756de1f12575c9d560cd103a1260ae80
NotProScope <iantkd7@gmail.com>
2023-07-17 21:53:45 -0700

    env

--- constants/constants.jsx
+++ constants/constants.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://mwqdtatdhlgqhyszfvwn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13cWR0YXRkaGxncWh5c3pmdnduIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTU2MTg3NiwiZXhwIjoyMDA1MTM3ODc2fQ.XWglUn5LLyYhY44j322snuM_jzebcYGhN8EAhE4jWIs';
+const supabaseUrl = PROJECT_URL;
+const supabaseKey = SERVICE_ROLE_KEY;
 const supabaseOptions = {auth: { persistSession: false }}; // Disable session persistence
 
 console.log(process.env)
