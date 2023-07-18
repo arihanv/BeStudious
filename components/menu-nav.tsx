b0da040534e4a023c0883a446fb0b85acceafc0d
arihanv <arihanvaranasi@gmail.com>
2023-07-17 23:45:08 -0500

    feat: Add Icons to Menubar

--- components/menu-nav.tsx
+++ components/menu-nav.tsx
@@ -2,6 +2,7 @@
 
 import React from "react"
 import Link from "next/link"
+import { Home, Medal, Rss, Settings, Users } from 'lucide-react';
 
 import {
   Menubar,
@@ -27,29 +28,39 @@ export default function MenuNav({}: Props) {
           <MenubarItem>
             {" "}
             <Link className="w-full" href="/">
-              Home{" "}
+              <div className="flex gap-1.5 items-center">
+                <Home width={15} /> Home
+              </div>
             </Link>
           </MenubarItem>
           <MenubarItem>
             {" "}
             <Link className="w-full" href="/feed">
-              Feed{" "}
+            <div className="flex gap-1.5 items-center">
+                <Rss width={15} /> Feed
+              </div>
             </Link>
           </MenubarItem>
           <MenubarItem>
             {" "}
             <Link className="w-full" href="/leaderboard">
-              Leaderboard{" "}
+            <div className="flex gap-1.5 items-center">
+                <Medal width={15} /> Leaderboard
+              </div>
             </Link>
           </MenubarItem>
           <MenubarItem>
             {" "}
             <Link className="w-full" href="/spaces">
-              Spaces{" "}
+            <div className="flex gap-1.5 items-center">
+                <Users width={15} /> Spaces
+              </div>
             </Link>
           </MenubarItem>
           <MenubarSeparator />
-          <MenubarItem>Settings</MenubarItem>
+          <MenubarItem> <div className="flex gap-1.5 items-center">
+                <Settings width={15} /> Settings
+              </div></MenubarItem>
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
