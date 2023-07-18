51ac7fe3055116f1a57ca65816df2d8bf6e8d977
arihanv <arihanvaranasi@gmail.com>
2023-07-17 23:00:50 -0500

    style: Fix Scrolling for Spaces

--- components/space/spaceGrid.tsx
+++ components/space/spaceGrid.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function SpaceGrid({}: Props) {
   return (
-    <div className="flex w-full max-w-[900px] grid-cols-1 flex-wrap justify-center gap-5 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:justify-start">
+    <div className="flex w-full max-w-[900px] grid-cols-1 flex-wrap justify-center gap-5 pb-16 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:justify-start">
       <Space />
       <Space />
       <Space />
