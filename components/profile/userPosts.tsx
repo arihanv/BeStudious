266cff545b42232640dec1e2533b2c0ff1d2a34c
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-07-20 22:00:27 -0400

    Feature: Add posts filter per profile

--- components/profile/userPosts.tsx
+++ components/profile/userPosts.tsx
@@ -1,14 +1,48 @@
+"use client"
+
 import React from 'react'
-import Posts from '../posts/posts'
+import Post from '../posts/post'
+import supabaseClient from '@/constants/constants'
 
 type Props = {
     user: any
 }
 
-export default function UserPosts({user}: Props) {
+type DataItem = {
+  name: string
+  href: string
+  created_at: string
+  profileUrl: string
+}
+
+export default async function UserPosts({user}: Props) {
+  let fetchedPosts = []
+
+  const { data, error } = await supabaseClient
+    .from("images")
+    .select()
+    .order("created_at", { ascending: false })
+    .eq("userId", user.id)
+
+  if (error || data === null) {
+    return console.error("Error fetching posts:", error.message)
+  }
+
+  for (let index in data as DataItem[]) {
+    fetchedPosts.push(
+      <Post
+        name={data[index].name}
+        imageUrl={data[index].href}
+        createdAt={data[index].created_at}
+        profileImgUrl={data[index].profileUrl}
+        key={index}
+      />
+    )
+  }
+
   return (
     <>
-    {/* <Posts posts={posts} setPosts={setPosts} /> */}
+    {fetchedPosts}
     </>
   )
 }
