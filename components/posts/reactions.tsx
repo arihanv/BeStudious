a5b1187046469aa77a0f41b49ab1d1a1b020f4bf
NotProScope <iantkd7@gmail.com>
2023-07-21 20:37:17 -0700

    changes

--- components/posts/reactions.tsx
+++ components/posts/reactions.tsx
@@ -15,26 +15,42 @@ type Props = {
   imgUrl: string
 }
 
-export default function Reactions({}: Props) {
+export default function Reactions({imgUrl}: Props) {
   const { user } = useUser()
 
   const handleThumbsUp = async (e) => {
     const { data: fetchData, error: fetchError } = await supabaseClient
       .from('images')
-      .select()
-      .eq('thumbsup', user.id)
-    
-    console.log(fetchData);
+      .select("thumbsup")
+      .eq('href', imgUrl)
 
-    const { data: insertData, error: insertError } = await supabaseClient 
-      .from("images")
-      .update([{ thumbsup: [ user.id ] }])
-      .eq("href", imgUrl)
-      .select()
-      if (insertError) {
-        console.error("Error occured", insertError);
+    if (!fetchData[0].thumbsup.find(user.id)) {
+      if (fetchData[0].thumbsup === null) {
+        const { data: insertData, error: insertError } = await supabaseClient 
+          .from("images")
+          .update([{ thumbsup: [ user.id ] }])
+          .eq("href", imgUrl)
+          .select()
+        if (insertError) {
+          console.error("Error occured...", insertError);
+        }
+      } else {
+        let array = fetchData[0].thumbsup;
+        array.push(user.id);
+        const { data: insertData, error: insertError } = await supabaseClient 
+          .from("images")
+          .update([{ thumbsup: array }])
+          .eq("href", imgUrl)
+          .select()
+        if (insertError) {
+          console.error("Error occured...", insertError);
+        }      
       }
-  // }
+      console.log("registered!");
+    } else {
+      console.log("already registered......");
+    }
+  }
 
   return (
     <div className="absolute bottom-0 right-0 rounded-tl-xl bg-black p-0">
