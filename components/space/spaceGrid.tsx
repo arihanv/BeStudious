14bc34ef71c6402cb3ff26e286b6c745a6466b0c
NotProScope <iantkd7@gmail.com>
2023-07-22 12:47:29 -0700

    change

--- components/space/spaceGrid.tsx
+++ components/space/spaceGrid.tsx
@@ -5,8 +5,14 @@ import supabaseClient from "@/constants/constants"
 
 type Props = {}
 
-export default function SpaceGrid({}: Props) {
-  supabaseClient.
+export default async function SpaceGrid({}: Props) {
+  const { data, error } = await supabaseClient
+    .from("spaces")
+    .select()
+
+  for (let space of data) {
+    console.log(space)
+  }
 
   return (
     <div className="flex w-full max-w-[900px] grid-cols-1 flex-wrap justify-center gap-5 pb-16 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:justify-start">
