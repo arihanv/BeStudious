9646888c99a4416b1c7c89299a2472a87d950a96
NotProScope <iantkd7@gmail.com>
2023-07-22 12:47:06 -0700

    changes

--- components/space/spaceGrid.tsx
+++ components/space/spaceGrid.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 
 import Space from "./space"
+import supabaseClient from "@/constants/constants"
 
 type Props = {}
 
 export default function SpaceGrid({}: Props) {
+  supabaseClient.
+
   return (
     <div className="flex w-full max-w-[900px] grid-cols-1 flex-wrap justify-center gap-5 pb-16 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:justify-start">
       <Space />
