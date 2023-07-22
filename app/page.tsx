eca16dada881ed2cc96468a9514399a682b56e8a
arihanv <arihanvaranasi@gmail.com>
2023-07-21 22:00:15 -0500

    feat: Update Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,27 @@
 import Link from "next/link"
 import { Github, Medal, Users } from "lucide-react"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
 
+import Post from "@/components/posts/post"
 export default function IndexPage() {
+
+  //get current time
+  const date = new Date().toISOString()
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex gap-4">
-        <Link href="/feed" rel="noreferrer" className={buttonVariants()}>
-          Go To Feed
-        </Link>
-        <Link
-          rel="noreferrer"
-          href="/leaderboard"
-          className={buttonVariants({ variant: "outline" })}
-        >
-          <Medal />
-        </Link>
-        <Link
-          href="https://github.com/arihanv/BeStudious"
-          rel="noreferrer"
-          className={buttonVariants({ variant: "outline" })}
-        >
-          <Github />
-        </Link>
-        <Link
-          href="/spaces"
-          rel="noreferrer"
-          className={buttonVariants({ variant: "outline" })}
-        >
-          <Users />
-        </Link>
+    <section className="container flex h-full flex-col items-center justify-center gap-10 pb-8 pt-6 md:py-10">
+      <div className="pointer-events-none -z-20 w-full max-w-[400px] cursor-default md:h-[400px]">
+      <Post name="Himothy" createdAt={date} imageUrl="https://cdn.discordapp.com/attachments/1123787740253786154/1132134958526369822/slZOAAAAFXRFWHRUaHVtYjo6U2l6ZQAzLjE0Nzg2TUL08octAAAAFnRFWHRUaHVtYjo6VVJJAGZpbGU6Ly9QTkc6pipnIgAAAABJRU5ErkJggg.png" profileImgUrl="https://i.guim.co.uk/img/media/4d65894cd46a446612b10fbc1337fe9394c6d291/0_100_3112_1867/master/3112.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=41118f27e08c30514642e4a4c6723db2"/>
+      <div className="dark-shadow" />
       </div>
-    </section>
+    <div className="flex h-full w-full max-w-[480px] flex-col items-center justify-center gap-2 pb-20 tracking-tight shadow-xl shadow-black">
+      <div className="w-full self-start pt-5 text-5xl font-bold lg:text-7xl ">
+       BeStudious.
+      </div>
+      <div className="w-full self-start text-base lg:text-xl">
+        Learn, Share, and Grow with an online community of students.
+      </div>
+    </div>
+  </section>
   )
 }
--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import { Menu, MoreVertical, Trash } from "lucide-react"
 import moment from "moment"
